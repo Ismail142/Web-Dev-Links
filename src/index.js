b0055520479c8d436b8dec8c1d92836ee01dba92
Ismail142 <yahyaismail142@gmail.com>
2023-11-06 22:18:15 +0000

    vercel analytics updated

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Styles/index.css';
 import App from './App';
-import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Analytics/>
   </React.StrictMode>
 );
 
